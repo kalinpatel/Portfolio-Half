630e1e9a81c75875495afe726f9fce3429af084e
kalinpatel <52113248+kalinpatel@users.noreply.github.com>
2020-10-29 13:25:45 -0500

    Update js.js

--- js.js
+++ js.js
@@ -2,23 +2,23 @@ $(window).on("scroll touchmove", function()
 {
     if ($(document).scrollTop() >= $("#one").position().top && $(document).scrollTop() < $("#about").position().top  ) 
     {
-        $('figure').css('background-image', 'url(assets/img/background.jpg)')
+        $('figure').css('background', 'url(assets/img/background.jpg)')
     };
     if ($(document).scrollTop() >= $("#about").position().top && $(document).scrollTop() < $("#service").position().top)
     {
-        $('figure').css('background-image', 'url(assets/img/img_two.jpg)')
+        $('figure').css('background', 'url(assets/img/img_two.jpg)')
     };
    if ($(document).scrollTop() >= $("#service").position().top && $(document).scrollTop() < $("#work").position().top ) 
    {
-        $('figure').css('background-image', 'url(assets/img/img_three.jpg)')
+        $('figure').css('background', 'url(assets/img/img_three.jpg)')
    };
    if ($(document).scrollTop() >= $("#work").position().top && $(document).scrollTop() < $("#contact").position().top ) 
    {
-        $('figure').css('background-image', 'url(assets/img/img_three.jpg)')
+        $('figure').css('background', 'url(assets/img/img_three.jpg)')
    };
    if ($(document).scrollTop() >= $("#contact").position().top) 
    {
-        $('figure').css('background-image', 'url(assets/img/img_four.jpg)')
+        $('figure').css('background', 'url(assets/img/img_four.jpg)')
    };
 
 });
