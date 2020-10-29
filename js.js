b51330d05d8aada2c5f2f465194504bf38566bd3
kalinpatel <52113248+kalinpatel@users.noreply.github.com>
2020-10-29 13:51:41 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,24 +1,24 @@
-// $(window).on("scroll touchmove", function() 
-// {
-//     if ($(document).scrollTop() >= $("#one").position().top && $(document).scrollTop() < $("#about").position().top  ) 
-//     {
-//         $('figure').css('background', 'url(assets/img/background.jpg)')
-//     };
-//     if ($(document).scrollTop() >= $("#about").position().top && $(document).scrollTop() < $("#service").position().top)
-//     {
-//         $('figure').css('background', 'url(assets/img/img_two.jpg)')
-//     };
-//    if ($(document).scrollTop() >= $("#service").position().top && $(document).scrollTop() < $("#work").position().top ) 
-//    {
-//         $('figure').css('background', 'url(assets/img/img_three.jpg)')
-//    };
-//    if ($(document).scrollTop() >= $("#work").position().top && $(document).scrollTop() < $("#contact").position().top ) 
-//    {
-//         $('figure').css('background', 'url(assets/img/img_three.jpg)')
-//    };
-//    if ($(document).scrollTop() >= $("#contact").position().top) 
-//    {
-//         $('figure').css('background', 'url(assets/img/img_four.jpg)')
-//    };
+$(window).on("scroll touchmove", function() 
+{
+    if ($(document).scrollTop() >= $("#one").position().top && $(document).scrollTop() < $("#about").position().top  ) 
+    {
+        $('figure').css('background', 'url(assets/img/background.jpg)')
+    };
+    if ($(document).scrollTop() >= $("#about").position().top && $(document).scrollTop() < $("#service").position().top)
+    {
+        $('figure').css('background', 'url(assets/img/img_two.jpg)')
+    };
+   if ($(document).scrollTop() >= $("#service").position().top && $(document).scrollTop() < $("#work").position().top ) 
+   {
+        $('figure').css('background', 'url(assets/img/img_three.jpg)')
+   };
+   if ($(document).scrollTop() >= $("#work").position().top && $(document).scrollTop() < $("#contact").position().top ) 
+   {
+        $('figure').css('background', 'url(assets/img/img_three.jpg)')
+   };
+   if ($(document).scrollTop() >= $("#contact").position().top) 
+   {
+        $('figure').css('background', 'url(assets/img/img_four.jpg)')
+   };
 
-// });
+});
