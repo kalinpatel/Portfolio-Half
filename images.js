6666de04c940b24258562cf449472f7bc507697a
Kalin Patel <kalinpatel@gmail.com>
2020-11-21 12:39:15 -0600

    Simplify image switching to only 2

--- images.js
+++ images.js
@@ -4,21 +4,22 @@ $(document).scroll(function () {
     var q = $('.thirdimg').position();
     var z = $('.lastimg').position();
     if (y > z.top) {
-        $('.image-4-co').show();
+        // $('.image-4-co').show();
+        $('.image-3-co').show();
         
 
     } 
-    else if (y > q.top) {
-        $('.image-3-co').show();
-        $('.image-4-co').hide();
+    // else if (y > q.top) {
+    //     $('.image-3-co').show();
+    //     $('.image-4-co').hide();
 
-    } 
-    else if (y > x.top) {
-        $('.image-2-co').show();
-        $('.image-3-co').hide();
-        $('.image-4-co').hide();
+    // } 
+    // else if (y > x.top) {
+    //     $('.image-2-co').show();
+    //     $('.image-3-co').hide();
+    //     $('.image-4-co').hide();
 
-    } 
+    // } 
     else {
         $('.image-2-co').hide();
         $('.image-3-co').hide();
