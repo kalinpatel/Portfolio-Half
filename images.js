5017c10fb75b65c796406f5a3227e4fa7f340eb9
Kalin Patel <kalinpatel@kalins-macbook.lan>
2020-11-26 22:41:42 -0600

    simplify images

--- images.js
+++ images.js
@@ -1,37 +1,37 @@
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    var x = $('.nextimg').position();
-    var q = $('.thirdimg').position();
-    var z = $('.lastimg').position();
-    if (y > z.top) {
-        // $('.image-4-co').show();
-        $('.image-3-co').show();
+// $(document).scroll(function () {
+//     var y = $(this).scrollTop();
+//     var x = $('.nextimg').position();
+//     var q = $('.thirdimg').position();
+//     var z = $('.lastimg').position();
+//     if (y > z.top) {
+//         $('.image-4-co').show();
+//         $('.image-3-co').show();
         
 
-    } 
-    // else if (y > q.top) {
-    //     $('.image-3-co').show();
-    //     $('.image-4-co').hide();
+//     } 
+//     else if (y > q.top) {
+//         $('.image-3-co').show();
+//         $('.image-4-co').hide();
 
-    // } 
-    // else if (y > x.top) {
-    //     $('.image-2-co').show();
-    //     $('.image-3-co').hide();
-    //     $('.image-4-co').hide();
+//     } 
+//     else if (y > x.top) {
+//         $('.image-2-co').show();
+//         $('.image-3-co').hide();
+//         $('.image-4-co').hide();
 
-    // } 
-    else {
-        $('.image-2-co').hide();
-        $('.image-3-co').hide();
-        $('.image-4-co').hide();
-    }
+//     } 
+//     else {
+//         $('.image-2-co').hide();
+//         $('.image-3-co').hide();
+//         $('.image-4-co').hide();
+//     }
 
-});
-// new ScrollMagic.Scene({
-//     triggerElement: "#work",
-//     triggerHook: 0.9,
-//     duration: "80%",
-//     offset: 50
-// })
-//     .setClassToggle("#img2", "showit")
-//     .addTo(controller);
+// });
+// // new ScrollMagic.Scene({
+// //     triggerElement: "#work",
+// //     triggerHook: 0.9,
+// //     duration: "80%",
+// //     offset: 50
+// // })
+// //     .setClassToggle("#img2", "showit")
+// //     .addTo(controller);
